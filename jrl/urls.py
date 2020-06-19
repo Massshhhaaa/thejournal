405bcdd62b17090382dc70679448817d63970e53
masha <57405750+Massshhhaaa@users.noreply.github.com>
2020-06-19 23:56:42 +0300

    k
    ксс хуета не подлючатеся

--- jrl/urls.py
+++ jrl/urls.py
@@ -17,7 +17,8 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 
+
 urlpatterns = [
     path('', include('mainapp.urls')),
     path('admin/', admin.site.urls),
-]
+    ]
